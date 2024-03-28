9c4187fba2bbabe05d539611e22c9bcf405161a3
Ralf Carneiro <119140528+RalfCarneiro@users.noreply.github.com>
2024-03-27 23:40:26 -0300

    Update loginScripts.js

--- loginScripts.js
+++ loginScripts.js
@@ -2,17 +2,13 @@ function showSignUpForm() {
     document.getElementById('loginForm').style.display = 'none';
     document.getElementById('signupForm').style.display = 'block';
 }
-
 document.getElementById('cadastroLink').addEventListener('click', function(event) {
     event.preventDefault(); 
     window.location.href = 'cadastro.html';
 });
-
 function login() {
     var email = document.getElementById('loginEmail').value;
     var password = document.getElementById('loginPassword').value;
-
-    // Verificação "fake" do login
     if (email === 'usuario@exemplo.com' && password === 'senha123') {
         alert('Login bem-sucedido! Redirecionando para a página principal...');
         window.location.href = 'index.html'; 
@@ -20,7 +16,6 @@ function login() {
         alert('E-mail ou senha incorretos. Por favor, tente novamente.');
     }
 }
-
 function cadastrar() {
     var email = document.getElementById('loginEmail').value;
     if (email === 'usuario@exemplo.com' && password === 'senha123') {
@@ -30,5 +25,4 @@ function cadastrar() {
     else {
         alert('E-mail ou senha incorretos. Por favor, tente novamente.');
     }
-    
 }
